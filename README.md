926f99a685894084bc004a366394c420eaec225d
Ewelina <ewelina.kieley@gmail.com>
2014-02-18 09:51:47 -0800

    test passing for create html input element story id is 65843836 q

---
+++ README.md
@@ -0,0 +1,27 @@
+# html_input_element
+
+[![Build Status](https://secure.travis-ci.org/user/html_input_element.png?branch=master)](http://travis-ci.org/user/html_input_element)
+
+
+## Installation
+
+Install with npm:
+
+```
+npm install --save html_input_element
+```
+
+
+## API
+
+### html_input_element()
+
+
+## Testing
+
+From the repo root:
+
+```
+npm install
+npm test
+```
---
+++ test/test.js
@@ -0,0 +1,39 @@
+var expect = require('chai').expect;
+var htmlInputElement = require('../html_input_element').htmlInputElement;
+
+
+describe('html_input_element object tests', function () {
+    'use strict';
+    var html_input_element;
+
+    beforeEach(function () {
+        html_input_element = new htmlInputElement("x", "2", "text");
+    });
+
+    describe('html_input_element()', function () {
+        it('creates an html input', function () {
+            expect(html_input_element).to.be.ok;
+        });
+        it('expect to have property "name"', function () {
+            expect(html_input_element).to.have.property('name');
+        });
+        it('expect to have property "value"', function () {
+            expect(html_input_element).to.have.property('value');
+        });
+        it('expect to have property "type"', function () {
+            expect(html_input_element).to.have.property('type');
+        });
+    });
+
+    describe('constructor', function () {
+        it('element should not change property name from "x"', function () {
+            expect(html_input_element.value).to.equal('x');
+        }),
+        it('element should not change property value from "2"', function () {
+            expect(html_input_element.value).to.equal('2');
+        })
+        it('element should not change property type from "text"', function () {
+            expect(html_input_element.type).to.equal('text');
+        })
+    });
+});
